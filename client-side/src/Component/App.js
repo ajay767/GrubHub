262600c403600272dfc016389cbc808538a616ca
AJAY YADAV <58135870+ajay767@users.noreply.github.com>
2020-12-02 14:55:00 +0530

    added testing

--- client-side/src/Component/App.js
+++ client-side/src/Component/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Test from "./test/test";
 
 function App() {
-	return <div>Will integrate Node API here</div>;
+	return <div>Will integrate Node API here!!</div>;
 }
 
 export default App;
